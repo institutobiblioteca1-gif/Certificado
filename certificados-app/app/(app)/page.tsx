5822704ac9e5472ce339ddd6fade53d29ea938a7
institutobiblioteca1-gif <institutobiblioteca1@gmail.com>
2026-09-16 20:43:10 -0300

    Update page.tsx

--- certificados-app/app/(app)/page.tsx
+++ certificados-app/app/(app)/page.tsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
 import { prisma } from "@/lib/db";
 
+export const dynamic = "force-dynamic";
+
 export default async function InicioPage() {
   const [totalLotes, totalCertificados, totalModelos] = await Promise.all([
     prisma.lote.count({ where: { deletedAt: null } }),
